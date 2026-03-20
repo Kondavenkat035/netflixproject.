212b54a285856dbcf3dc9b97b8e8c6c5e0414887
Kondavenkat035 <kondavenkat035@gmail.com>
2026-03-20 13:24:18 +0530

    LoginServlet.java

--- src/main/java/com/example/LoginServlet.java
+++ src/main/java/com/example/LoginServlet.java
@@ -7,7 +7,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/login")
 public class LoginServlet extends HttpServlet {
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
